6c6598c5cba2805a2b3f95d6a775aa0da44902b6
nonato-coo <ajrodas624@gmail.com>
2026-04-03 13:51:31 -0600

    Implementado el logger

--- src/logger.c
+++ src/logger.c
@@ -1,3 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <time.h>
 #include "logger.h"
+#include "protocol.h"
 
-/* por implementar - Integrante 2 */
+void log_event(char *message) {
+    FILE *f = fopen(LOG_FILE, "a");
+    if (f == NULL) {
+        perror("Error opening log fie");
+        return;
+    }
+
+    time_t now = time(NULL);
+    struct tm *t = localtime(&now);
+    char timestamp[64];
+    strftime(timestamp, sizeof(timestamp), "%Y-%m-%d %H:%M:%S", t);
+
+    fprintf(f, "[%s] %s\n", timestamp, message);
+    fflush(f);
+    fclose(f);
+}
